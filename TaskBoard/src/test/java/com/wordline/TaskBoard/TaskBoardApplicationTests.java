2f54947583c195d679376e8be2ece46786bb6d91
axelASas <axelnystedtl@gmail.com>
2025-11-17 08:51:59 +0100

    initial commit

---
+++ TaskBoard/src/test/java/com/wordline/TaskBoard/TaskBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordline.TaskBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
